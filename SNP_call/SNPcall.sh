f6bcb97071166fb47be92f946b2b9685afe8f1fa
YuFeng <31508684+Fengyaa@users.noreply.github.com>
2022-02-05 09:48:03 -0600

    Update SNPcall.sh

--- SNP_call/SNPcall.sh
+++ SNP_call/SNPcall.sh
@@ -44,3 +44,34 @@ cat samplelist| parallel "java -Xms4g -Xmx20g -jar /export/GenomeAnalysisTK-3.5/
 cd gvcfs
 java -Xms4g -Xmx20g -jar /export/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar -T CombineGVCFs -R ../Did.genome.fa -o Did_all.g.vcf.gz -V BGC07_flt_MD.g.vcf.gz -V CPT16_flt_MD.g.vcf.gz -V LZ15_flt_MD.g.vcf.gz -V MLP03_flt_MD.g.vcf.gz -V BGC13_flt_MD.g.vcf.gz -V HTS20_flt_MD.g.vcf.gz -V LZ22_flt_MD.g.vcf.gz -V  MLP04_flt_MD.g.vcf.gz -V CJZ06_flt_MD.g.vcf.gz -V HTS23_flt_MD.g.vcf.gz -V LZ25_flt_MD.g.vcf.gz -V MLP05_flt_MD.g.vcf.gz -V CJZ08_flt_MD.g.vcf.gz -V LZ06_flt_MD.g.vcf.gz -V LZ26_flt_MD.g.vcf.gz -V XMF11_flt_MD.g.vcf.gz -V CJZ14_flt_MD.g.vcf.gz -V LZ09_flt_MD.g.vcf.gz -V LZ31_flt_MD.g.vcf.gz -V CJZ16_flt_MD.g.vcf.gz -V LZ12_flt_MD.g.vcf.gz -V MLP01_flt_MD.g.vcf.gz -V CPT13_flt_MD.g.vcf.gz -V LZ14_flt_MD.g.vcf.gz -V MLP02_flt_MD.g.vcf.gz
 
+cd /scratch/yufeng/reseq/Dis_recall
+bowtie2-build Dis.genome.fa Dis.genome
+cat Dis_list1|parallel "java -jar /scratch/yufeng/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 reads/{}_1_clean.fq.gz reads/{}_2_clean.fq.gz reads/{}.1_trimmed.fq.gz reads/{}.1_unpaired.fq.gz reads/{}.2_trimmed.fq.gz reads/{}.2_unpaired.fq.gz ILLUMINACLIP:/scratch/yufeng/softwares/Trimmomatic-0.39/adapters/TruSeq2-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 && rm reads/{}_1_clean.fq.gz reads/{}_2_clean.fq.gz reads/{}.1_unpaired.fq.gz reads/{}.2_unpaired.fq.gz && bowtie2 -x Dis.genome -1 reads/{}.1_trimmed.fq.gz -2 reads/{}.2_trimmed.fq.gz > alignments/{}.sam && samtools view -Sb alignments/{}.sam| samtools sort - > alignments/{}.bam && rm alignments/{}.sam"
+
+cd alignments
+##add bam tags
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM01_L4.bam O=BTM01_R.bam RGID=BTM01_L4 RGLB=BTM01 RGPL=illumina RGPU=L4 RGSM=BTM01
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM05_L2.bam O=BTM05_R.bam RGID=BTM05_L2 RGLB=BTM05 RGPL=illumina RGPU=L2 RGSM=BTM05
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM06_L4.bam O=BTM06_R.bam RGID=BTM06_L4 RGLB=BTM06 RGPL=illumina RGPU=L4 RGSM=BTM06
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM08_L3.bam O=BTM08_L3_R.bam RGID=BTM08_L3 RGLB=BTM08 RGPL=illumina RGPU=L3 RGSM=BTM08
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM08_L4.bam O=BTM08_L4_R.bam RGID=BTM08_L4 RGLB=BTM08 RGPL=illumina RGPU=L4 RGSM=BTM08
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BTM12_L3.bam O=BTM12_R.bam RGID=BTM12_L3 RGLB=BTM12 RGPL=illumina RGPU=L3 RGSM=BTM12
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BX02_L3.bam O=BX02_L3_R.bam RGID=BX02_L3 RGLB=BX02 RGPL=illumina RGPU=L3 RGSM=BX02
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BX02_L4.bam O=BX02_L4_R.bam RGID=BX02_L4 RGLB=BX02 RGPL=illumina RGPU=L4 RGSM=BX02
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BX09_L3.bam O=BX09_R.bam RGID=BX09_L3 RGLB=BX09 RGPL=illumina RGPU=L3 RGSM=BX09
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BX11_L4.bam O=BX11_R.bam RGID=BX11_L4 RGLB=BX11 RGPL=illumina RGPU=L4 RGSM=BX11
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=BX12_L2.bam O=BX12_R.bam RGID=BX12_L2 RGLB=BX12 RGPL=illumina RGPU=L2 RGSM=BX12
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=DLL07_L2.bam O=DLL07_R.bam RGID=DLL07_L2 RGLB=DLL07 RGPL=illumina RGPU=L2 RGSM=DLL07
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=DLL12_L3.bam O=DLL12_R.bam RGID=DLL12_L3 RGLB=DLL12 RGPL=illumina RGPU=L3 RGSM=DLL12
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar AddOrReplaceReadGroups I=DLL16_L2.bam O=DLL16_R.bam RGID=DLL16_L2 RGLB=DLL16 RGPL=illumina RGPU=L2 RGSM=DLL16
+
+##merge bam files
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar MergeSamFiles I=BTM08_L3_R.bam I=BTM08_L4_R.bam O=BTM08_R.bam
+java -jar -Xmx4g /scratch/yufeng/softwares/picard.jar MergeSamFiles I=BX02_L3_R.bam I=BX02_L4_R.bam O=BX02_R.bam
+cd ..
+cat Dis_samplelist1|parallel "samtools flagstat alignments/{}_R.bam >alignments/{}_R.bam.flagstat && samtools view -q 20 -f 0x0002 -F 0X0004 -F 0X0008 -b alignments/{}_R.bam > alignments/{}_flt.bam"
+cat Dis_samplelist1|parallel "java -jar /scratch/yufeng/softwares/picard.jar MarkDuplicates I=alignments/{}_flt.bam O=alignments/{}_flt_MD.bam M=alignments/{}_flt_MD.log && rm alignments/{}_R.bam"
+java -jar /scratch/yufeng/softwares/picard.jar CreateSequenceDictionary R=Dis.genome.fa O=Dis.genome.dict
+samtools faidx Dis.genome.fa
+cat Dis_samplelist1| parallel "samtools index alignments/{}_flt_MD.bam"
+cat Dis_samplelist1| parallel "java -Xms4g -Xmx20g -jar /export/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar -T HaplotypeCaller -R Did.genome.fa -I alignments/{}_flt_MD.bam -ERC GVCF -o ./gvcfs/{}_flt_MD.g.vcf.gz"
